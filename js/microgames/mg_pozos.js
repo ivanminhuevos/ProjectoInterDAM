5579596466b65769ba89cdd47ee9cbf43293af3f
UsernameFiller923 <susmilk12@gmail.com>
2026-02-26 08:51:42 +0100

    Woah! nuevo juego (WIP)

---
+++ js/microgames/mg_pozos.js
@@ -0,0 +1,123 @@
+(function () {
+    var mg = new Microgame("pozos", "¡Busca el agua potable!");
+
+    var potableWell = 0;
+    var wells = [
+        { x: 100, y: 300, w: 150, h: 200, hover: 0 },
+        { x: 325, y: 300, w: 150, h: 200, hover: 0 },
+        { x: 550, y: 300, w: 150, h: 200, hover: 0 }
+    ];
+
+    var timeLeft = 5;
+    var state = "playing"; // "playing", "won", "lost"
+    var stateTime = 0;
+
+    mg.init = function () {
+        potableWell = Math.floor(Math.random() * 3);
+        timeLeft = 5;
+        state = "playing";
+        stateTime = 0;
+
+        console.log("Minijuego Pozos: Pozo potable es " + potableWell);
+
+        for (var i = 0; i < wells.length; i++) {
+            wells[i].hover = 0;
+        }
+    }
+
+    mg.think = function (dt) {
+        stateTime += dt;
+
+        if (state === "playing") {
+            timeLeft -= dt;
+            if (timeLeft <= 0) {
+                timeLeft = 0;
+                state = "lost";
+                stateTime = 0;
+            }
+
+            for (var i = 0; i < wells.length; i++) {
+                var w = wells[i];
+                var isOver = Mouse_InBox(w.x, w.y, w.w, w.h);
+
+                if (isOver) {
+                    w.hover = Math.min(w.hover + dt * 10, 1);
+
+                    if (Mouse_GetLeftDown()) {
+                        if (i === potableWell) {
+                            state = "won";
+                            stateTime = 0;
+                        } else {
+                            state = "lost";
+                            stateTime = 0;
+                        }
+                    }
+                } else {
+                    w.hover = Math.max(w.hover - dt * 10, 0);
+                }
+            }
+        } else {
+            // After 1.5 seconds of showing feedback, proceed
+            if (stateTime > 1.5) {
+                if (state === "won") {
+                    mg.win();
+                } else {
+                    PIDAM_BeginInterlude(); // Manual fail transition
+                }
+            }
+        }
+    }
+
+    mg.draw = function () {
+        Canvas_Clear(10, 10, 25);
+
+        // Draw Timer
+        var timerWidth = (timeLeft / 5) * Canvas_GetWidth();
+        Canvas_SetDrawColor(255, 255, 0);
+        Canvas_DrawRect(0, 0, timerWidth, 10);
+
+        // Draw instructions or feedback
+        var msg = mg.desc;
+        var msgColor = [255, 255, 255];
+
+        if (state === "won") {
+            msg = "¡Bien!";
+            msgColor = [0, 255, 0];
+        } else if (state === "lost") {
+            msg = "Uh oh...";
+            msgColor = [255, 0, 0];
+        }
+
+        Canvas_SetFont("48px Outfit, sans-serif");
+        Canvas_SetDrawColor(msgColor[0], msgColor[1], msgColor[2]);
+        Canvas_DrawTextAlign(msg, Canvas_GetWidth() / 2, 100, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER);
+
+        // Draw Wells
+        for (var i = 0; i < wells.length; i++) {
+            var w = wells[i];
+            var hoverScale = 1 + w.hover * 0.05;
+            var drawW = w.w * hoverScale;
+            var drawH = w.h * hoverScale;
+            var drawX = w.x - (drawW - w.w) / 2;
+            var drawY = w.y - (drawH - w.h) / 2;
+
+            Canvas_SetDrawColor(30, 30, 60);
+            Canvas_DrawRect(drawX, drawY, drawW, drawH);
+
+            var r = i === potableWell ? 0 : 180;
+            var g = i === potableWell ? 180 : 0;
+            var b = 255;
+
+            Canvas_SetDrawColorA(r, g, b, 0.2 + w.hover * 0.3);
+            Canvas_DrawRect(drawX + 10, drawY + 10, drawW - 20, drawH - 20);
+
+            Canvas_SetDrawColor(100, 100, 150);
+            Canvas_DrawRect(drawX, drawY, drawW, 20);
+
+            if (w.hover > 0 && state === "playing") {
+                Canvas_SetDrawColorA(255, 255, 255, w.hover * 0.3);
+                Canvas_DrawRect(drawX, drawY, drawW, drawH);
+            }
+        }
+    }
+})();
--- js/pidam.js
+++ js/pidam.js
@@ -6,16 +6,24 @@ function PIDAM_InterludeDraw() {
     var currMG = Microgames_GetActive();
 
     var timeSince = (Date.now() - onMinigameStart) / 1000;
-    timeSince = timeSince * 16;
-    var szDelta = Math.min(timeSince, 50);
+    var animTime = timeSince * 32;
+    var fontSize = Math.min(animTime, 60); // Cap font size
 
-    Canvas_SetFont(timeSince + "px sans-serif");
+    Canvas_Clear(0, 0, 0); // Clear to prevent ghosting
+    Canvas_SetFont(fontSize + "px Outfit, sans-serif");
     Canvas_SetDrawColor(255, 255, 255);
     Canvas_DrawTextAlign(currMG.desc, Canvas_GetWidth() / 2, Canvas_GetHeight() / 2, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER);
 }
 
 function PIDAM_InterludeThink() {
+    var timeSince = (Date.now() - onMinigameStart) / 1000;
 
+    // After 2 seconds, pick a new random microgame
+    if (timeSince > 2) {
+        var nextMG = Microgames_GetRandom();
+        Microgames_SetActive(nextMG);
+        onMinigame = true;
+    }
 }
 
 function PIDAM_BeginInterlude() {
@@ -34,7 +42,7 @@ function PIDAM_Init() {
     Canvas_Init();
 
     Canvas_Clear(0, 0, 0);
-    
+
     canvas = Canvas_GetCanvas();
 
     prevTime = Date.now();
@@ -42,8 +50,8 @@ function PIDAM_Init() {
     requestAnimationFrame(function update() {
         deltaTime = (Date.now() - prevTime) / 1000;
         Util_Internal_SetDeltaTime(deltaTime);
-        
-        if(onMinigame) {
+
+        if (onMinigame) {
             Microgames_ThinkActive();
             Microgames_DrawActive();
         } else {
@@ -57,10 +65,10 @@ function PIDAM_Init() {
 }
 
 
-window.onload = function() {
+window.onload = function () {
     PIDAM_Init();
 
-    Microgames_SetActive("arbol");
+    Microgames_SetActive("pozos");
     onMinigame = true;
 
     //PIDAM_BeginInterlude();
